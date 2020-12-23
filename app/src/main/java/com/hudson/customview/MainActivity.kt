abb55ca7d0b3b12b5c4a789aa667c49155eeb450
Hudson <2216274479@qq.com>
2020-12-23 13:18:50 +0800

    初始化

---
+++ app/src/main/java/com/hudson/customview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hudson.customview
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.hudson.customview.gesturelock.GestureLockActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun jump(view: View) {
+        GestureLockActivity.start(this)
+    }
+}
