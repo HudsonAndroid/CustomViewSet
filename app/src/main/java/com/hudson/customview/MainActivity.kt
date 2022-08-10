e6470e177ea3ea609f3860eee69f809dcaba341b
Hudson <2216274479@qq.com>
2022-08-10 21:19:26 +0800

    Android: APT-自动完成页面注入处理

--- app/src/main/java/com/hudson/customview/MainActivity.kt
+++ app/src/main/java/com/hudson/customview/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.hudson.customview
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.hudson.customview.diagramview.percentview.sample.PercentActivity
-import com.hudson.customview.diagramview.table.TableDiagramActivity
+import com.hudson.apt.CustomViewSetPageRegister
+import com.hudson.customview.wraplayout.WrapLayout
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun jump(view: View) {
-        startTargetPage<PercentActivity>()
+        val parent = findViewById<WrapLayout>(R.id.wrap_container)
+
+        val pages = CustomViewSetPageRegister().pages
+        pages.keys.forEach { clazz ->
+            parent.addView(Button(this).apply {
+                text = pages[clazz]
+                setOnClickListener {
+                    startActivity(Intent(this@MainActivity, clazz))
+                }
+            })
+        }
     }
 }
